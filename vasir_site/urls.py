78aef9358ca0c6f943011e66ac0269255ab4add6
Erik Hazzard <enoex1@gmail.com>
2011-03-22 18:37:24 +0000

    Updated book info

--- vasir_site/urls.py
+++ vasir_site/urls.py
@@ -14,9 +14,9 @@ urlpatterns = patterns('',
         'vasir_site.views.openlayers_book',
         name='page_openlayers_book'),
 
-    url(r'^openlayers_book/files/$',
-        'vasir_site.views.openlayers_book_files',
-        name='page_openlayers_book_files'),
+    url(r'openlayers_book/files/$',
+        'vasir_site.views.openlayers_book',
+        name='page_openlayers_book'),
 
     url(r'^about/$',
         'vasir_site.views.about',
